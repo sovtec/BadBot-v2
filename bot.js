9a6e6f1bd961c2ca98bd9289cf61191a8aeeb155
hrdu_ <ragnarbergseth@yahoo.no>
2022-12-06 14:25:26 +0100

    Fixed command collection

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-const { token } = require("./config.json");
-const path = require("node:path");
+
 const fs = require("node:fs");
+const path = require("node:path");
+const { token } = require("./config.json");
 const {
   Client,
   GatewayIntentBits,
@@ -18,6 +19,34 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
   ],
 });
+
+client.commands = new Collection();
+
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
 
 client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
@@ -36,22 +65,6 @@ client.on(Events.InteractionCreate, async (interaction) => {
     });
   }
 });
-
-client.commands = new Collection();
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs
-  .readdirSync(eventsPath)
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
 
 //This is being called from ready.js
 /* client.on("ready", () => {
