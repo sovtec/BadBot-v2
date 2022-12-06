4f9b42e26b8bd056f7d4747fcb6b0c1f4a8a280a
hrdu_ <ragnarbergseth@yahoo.no>
2022-12-06 10:58:02 +0100

    Fixed slash commands and cleaned up

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
+const path = require("node:path");
+const fs = require("node:fs");
 const {
   Client,
   GatewayIntentBits,
   Collection,
   messageLink,
   User,
+  Events,
 } = require("discord.js");
 const client = new Client({
   intents: [
@@ -18,6 +21,18 @@ const client = new Client({
 
 client.login(process.env.BOT_TOKEN);
 
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
+}
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -26,43 +41,23 @@ client.on("messageCreate", (message) => {
   console.log(`Keylog: ` + `${client.user.tag}: ` + message.content);
 });
 
-const { REST, Routes } = require("discord.js");
-const { clientId, guildId, token } = require("./config.json");
-const fs = require("node:fs");
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+  const command = interaction.client.commands.get(interaction.commandName);
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
-}
-
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: "10" }).setToken(token);
-
-// and deploy your commands!
-(async () => {
-  try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
-
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands }
-    );
-
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
-  } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
   }
-})();
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
