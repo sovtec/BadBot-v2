ec76a29572a4ad0ae574308805f6f2b4df91b897
hrdu_ <ragnarbergseth@yahoo.no>
2022-12-06 13:19:36 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 const { Events } = require("discord.js");
 
 module.exports = {
-  name: "interactionCreate",
+  name: "InteractionCreate",
   async execute(interaction) {
     if (!interaction.isChatInputCommand()) return;
 
